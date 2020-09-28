4cdb9a36139795af16478d211d3af2780c2422bb
YeChang <337894180@qq.com>
2020-09-28 20:40:23 +0800

    呵呵

--- githubapps/index.js
+++ githubapps/index.js
@@ -1,5 +1,5 @@
-const token = "34204a9673d69e86229285248e4597bf57d4d004";
-const fs = require("fs");
+// http
+const token = "aaaaf77fea9bb604fddb94c63fd3bcfc05fdace6";
 const axios = require("axios");
 const http = axios.create({
   baseURL: "https://api.github.com/",
@@ -9,28 +9,58 @@ const http = axios.create({
   },
 });
 
-let items = JSON.parse(
-  fs.readFileSync("data.json", "utf-8", function (err, data) {
-    if (err) {
-      console.error(err);
-    }
+// console.log(items);
+
+const mongoose = require("mongoose");
+mongoose.connect(`mongodb://127.0.0.1:27017/Test-22`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Item = mongoose.model(
+  "Item",
+  new mongoose.Schema({
+    name: String,
+    url: String,
+    urls: Array,
   })
 );
-// console.log(items);
-async function doit() {
-  // console.log(items);
-  // for (const item of items) {
-  let item = items[0];
-  console.log(item);
-  let url = `/search/issues?q=repo:${item.name}+special+character`;
-  // console.log(url);
-  const res = await http.get(url);
-  item.urls = res.data.items;
-  console.log(res.headers);
-  console.log(res.data.items.length);
-  // }
+
+async function doit(items, count) {
+  for (const item of items) {
+    let url = `/search/issues?q=repo:${item.name}+special+character`;
+    const res = await http.get(url);
+    let temp = {
+      name: item.name,
+      url: item.url,
+      urls: res.data.items,
+    };
+    await Item.create(temp);
+    // console.log(res.data.items.length);
+  }
 }
 
-doit();
+function __main() {
+  const fs = require("fs");
 
-// fs.writeFileSync("test.json", JSON.stringify(items, "", "\t"));
+  var items = JSON.parse(
+    fs.readFileSync("data.json", "utf-8", function (err, data) {
+      if (err) {
+        console.error(err);
+      }
+    })
+  );
+
+  var timesRun = 0;
+  var count = 0;
+  var interval = setInterval(function () {
+    timesRun++;
+    if (timesRun === 30) {
+      clearInterval(interval);
+    }
+    doit(items.slice(0, 30), count);
+    //do whatever here..
+  }, 61000);
+}
+
+__main();
